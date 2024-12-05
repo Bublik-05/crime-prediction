3507808985dc8cd703c7c3e2d3e5446b67b4c90f
Abylay <140843960+Bublik-05@users.noreply.github.com>
2024-12-05 19:38:49 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,20 +7,42 @@ app = Flask(__name__)
 CORS(app)  # Разрешаем CORS для всех маршрутов
 
 # Загрузка моделей
-rf_model = joblib.load('rf_model.joblib')
-gb_model = joblib.load('gb_model.joblib')
+model = joblib.load('crime_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+selected_features = ['assaultPerPop', 'rapes', 'robbbPerPop', 'rapesPerPop', 
+                            'PctKidsBornNeverMar', 'murdPerPop', 'NumKidsBornNeverMar',
+                            'OwnOccMedVal', 'MalePctNevMarr', 'HousVacant', 'racePctHisp',
+                            'PctFam2Par', 'PctPersOwnOccup', 'larcPerPop']
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json
-    input_data = pd.DataFrame([data])  # Конвертируем JSON в DataFrame
-    
-    # Предсказание модели
-    rf_pred = rf_model.predict(input_data)[0]
-    gb_pred = gb_model.predict(input_data)[0]
-    ensemble_pred = (rf_pred + gb_pred) / 2
+    # Получаем данные в формате JSON
+    data = request.get_json()
 
-    return jsonify({'prediction': ensemble_pred})
+    # Проверяем наличие всех необходимых признаков
+    missing_features = [feature for feature in selected_features if feature not in data]
+    if missing_features:
+        return jsonify({'error': f'Отсутствуют следующие признаки: {missing_features}'}), 400
+
+    # Преобразуем данные в DataFrame
+    input_data = pd.DataFrame([data], columns=selected_features)
+
+    # Преобразуем типы данных в float
+    input_data = input_data.astype(float)
+
+    # Масштабируем данные
+    input_data_scaled = scaler.transform(input_data)
+
+    # Предсказание
+    prediction = model.predict(input_data_scaled)
+
+    # Обратное преобразование (если применяли np.sqrt к целевой переменной)
+    prediction = prediction ** 2
+
+    # Возвращаем результат
+    return jsonify({'prediction': prediction[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
